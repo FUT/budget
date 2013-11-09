e517f3a445de363e1193bd13d1d29f85707cc070
Yury Kaliada <fut.wrk@gmail.com>
2013-11-10 02:49:36 +0300

    comment out charts
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @transfers = current_user.transfers.order(:created_at).with_total
-    @chart = Chart.basic_line @transfers
+    # @chart = Chart.basic_line @transfers
   end
 end
